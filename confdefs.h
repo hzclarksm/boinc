6c13e0525ab15c99bd18e86629420ca346a4494f
Steven Clark <clarks@purdue.edu>
2019-10-08 17:15:02 -0400

    Files created or modified by build server

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "BOINC"
+#define PACKAGE_TARNAME "boinc"
+#define PACKAGE_VERSION "7.13.0"
+#define PACKAGE_STRING "BOINC 7.13.0"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define PACKAGE "boinc"
+#define VERSION "7.13.0"
